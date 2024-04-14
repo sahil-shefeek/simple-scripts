47f66b5c9848f6ca0df45756bf65ababeedbccbf
Sahil Shefeek <sahilms345@gmail.com>
2024-04-14 19:37:26 +0530

    minor update

--- random-scripts/sysconf.sh
+++ random-scripts/sysconf.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-WORK_DIR = $(pwd)
 cat << EOF
 ----------------------
 System configurations
@@ -9,6 +8,6 @@ Total number of users: $(who -q | grep users | awk -F'=' '{print $2}')
 Current shell: $SHELL
 OS: $(uname -o)
 Home directory: $HOME
-Current working dir: $WORK_DIR
+Current working dir: $(pwd)
 Current PATH: $PATH
 EOF
